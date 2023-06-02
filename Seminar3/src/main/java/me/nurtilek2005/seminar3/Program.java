4da93d904423e3729cf9029983c031276d578615
Nurtilek <nurtileks2005@gmail.com>
2023-06-02 15:15:23 +0600

    added seminar_3

---
+++ Seminar3/src/main/java/me/nurtilek2005/seminar3/Program.java
@@ -0,0 +1,11 @@
+package me.nurtilek2005.seminar3;
+
+public class Program {
+
+    public int a;
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Seminar3/src/main/java/me/nurtilek2005/seminar3/task2/Freelancer.java
@@ -0,0 +1,9 @@
+package me.nurtilek2005.seminar3.task2;
+
+// 20 * 8
+
+/**
+ * TODO: Спроектировать класс Freelancer самостоятельно в рамках домашнего задания
+ */
+public class Freelancer {
+}
