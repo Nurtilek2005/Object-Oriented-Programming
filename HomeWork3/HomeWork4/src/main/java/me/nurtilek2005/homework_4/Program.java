20fa581ace7a0d3e397d36504384bb4aaf259d61
Nurtilek <nurtileks2005@gmail.com>
2023-06-10 19:59:31 +0600

    update

--- HomeWork3/HomeWork4/src/main/java/me/nurtilek2005/homework_4/Program.java
+++ HomeWork3/HomeWork4/src/main/java/me/nurtilek2005/homework_4/Program.java
@@ -2,7 +2,6 @@ package me.nurtilek2005.homework_4;
 
 public class Program {
     public void run() {
-        System.out.println("Hello world!");
     }
 
     public static void main(String[] args) {
