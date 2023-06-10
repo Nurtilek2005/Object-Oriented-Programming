5d79ce6dd2e059f4535cf201129d3b135fd1397b
Nurtilek <nurtileks2005@gmail.com>
2023-06-10 19:59:06 +0600

    finished HomeWork3

---
+++ HomeWork3/HomeWork4/src/main/java/me/nurtilek2005/homework_4/Program.java
@@ -0,0 +1,12 @@
+package me.nurtilek2005.homework_4;
+
+public class Program {
+    public void run() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        Program program = new Program();
+        program.run();
+    }
+}
